ef70260b828488f77a9c986247ba07a9267a4698
sandra williams <sannwilliams93@yahoo.com>
2018-07-11 15:36:08 -0500

    complete class work

---
+++ example1.py
@@ -0,0 +1,17 @@
+import os
+os.system ('clear')
+
+bandMate = "riNgo"
+print ("the name is this long" , len(bandMate))
+print(bandMate.upper())
+print(bandMate.lower())
+print (bandMate[2])
+print(bandMate[2:4])
+print (bandMate[0:1])
+
+print(bandMate.capitalize())
+print(bandMate.swapcase())
+name = "sann"
+print ("the name is sann" , len(name))
+print('sann'.upper())
+name2 = 'sann','bill'
